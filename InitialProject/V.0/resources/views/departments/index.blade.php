956c0992a6ef520aac2ebd9b438943f6f881337b
Tanthai-O <tanthai.o@kkumail.com>
2025-03-13 00:09:46 +0700

    edit change language

--- InitialProject/V.0/resources/views/departments/index.blade.php
+++ InitialProject/V.0/resources/views/departments/index.blade.php
@@ -16,11 +16,9 @@
         </div>
         @endif
         <div class="card">
-            <div class="card-header">Departments
+            <div class="card-header">{{ __('departments.title') }}
                 @can('departments-create')
-
-                <a class="btn btn-primary" href="{{ route('departments.create') }}">New department</a>
-
+                <a class="btn btn-primary" href="{{ route('departments.create') }}">{{ __('departments.new') }}</a>
                 @endcan
             </div>
             <div class="card-body">
@@ -28,44 +26,42 @@
                     <thead class="thead-dark">
                         <tr>
                             <th>#</th>
-                            <th>Name</th>
-                            <th width="280px">Action</th>
+                            <th>{{ __('departments.name') }}</th>
+                            <th width="280px">{{ __('departments.action') }}</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($data as $key => $department)
                         <tr>
                             <td>{{ $department->id }}</td>
-                            <td>{{ $department->department_name_th }}</td>
+                            <td>
+    @php
+        $locale = App::getLocale() === 'zh' ? 'cn' : App::getLocale();
+        $nameField = 'department_name_' . $locale;
+        $departmentName = $department->$nameField ?? $department->department_name_en ?? $department->department_name_th;
+    @endphp
+    {{ $departmentName }}
+</td>
                             <td>
                                 <form action="{{ route('departments.destroy',$department->id) }}" method="POST">
                                     
-
-                                    <a class="btn btn-outline-primary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="view" href="{{ route('departments.show',$department->id) }}"><i class="mdi mdi-eye"></i></a>
+                                    <a class="btn btn-outline-primary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="{{ __('departments.view') }}" href="{{ route('departments.show',$department->id) }}"><i class="mdi mdi-eye"></i></a>
 
                                     @can('departments-edit')
-
-                                    <a class="btn btn-outline-primary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Edit" href="{{ route('departments.edit',$department->id) }}"><i class="mdi mdi-pencil"></i></a>
-
+                                    <a class="btn btn-outline-primary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="{{ __('departments.edit') }}" href="{{ route('departments.edit',$department->id) }}"><i class="mdi mdi-pencil"></i></a>
                                     @endcan
 
-
                                     @can('departments-delete')
-                                    <!-- {!! Form::open(['method' => 'DELETE','route' => ['departments.destroy', $department->id],'style'=>'display:inline']) !!}
-                                    {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit','class' => 'btn btn-danger btn-sm rounded-0','type'=>'button','data-toggle'=>'tooltip' ,'data-placement'=>'top', 'title'=>'Delete']) !!}
-                                    {!! Form::close() !!} -->
                                     @csrf
                                     @method('DELETE')
 
                                     <li class="list-inline-item">
-                                        <button class="btn btn-outline-danger btn-sm show_confirm" type="submit" data-toggle="tooltip" data-placement="top" title="Delete"><i class="mdi mdi-delete"></i></button>
+                                        <button class="btn btn-outline-danger btn-sm show_confirm" type="submit" data-toggle="tooltip" data-placement="top" title="{{ __('departments.delete') }}"><i class="mdi mdi-delete"></i></button>
                                     </li>
-
 
                                     @endcan
                                 </form>
                             </td>
-
                         </tr>
                         @endforeach
                     </tbody>
@@ -75,21 +71,22 @@
         </div>
     </div>
 </div>
+
 <script type="text/javascript">
     $('.show_confirm').click(function(event) {
         var form = $(this).closest("form");
         var name = $(this).data("name");
         event.preventDefault();
         swal({
-                title: `{{ __('message.are_you_sure') }}`,
-                text: "{{ __('message.delete_warning') }}",
+                title: `{{ __('departments.confirm_title') }}`,
+                text: "{{ __('departments.confirm_text') }}",
                 icon: "warning",
                 buttons: true,
                 dangerMode: true,
             })
             .then((willDelete) => {
                 if (willDelete) {
-                    swal("{{ __('message.deleted_successfully') }}", {
+                    swal("{{ __('departments.deleted') }}", {
                         icon: "success",
                     }).then(function() {
                         location.reload();
@@ -99,4 +96,4 @@
             });
     });
 </script>
-@endsection
+@endsection 
